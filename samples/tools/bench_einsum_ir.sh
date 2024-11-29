25577264a5da22c637500a7b330aa127961eeb8a
Alexander Nikolas Breuer <alex.breuer@uni-jena.de>
2024-11-29 16:41:39 +0100

    Added einsum tree versions of benchmarks.

--- samples/tools/bench_einsum_ir.sh
+++ samples/tools/bench_einsum_ir.sh
@@ -7,21 +7,24 @@ while getopts 'pe:l:r:b:d:k:hv' flag; do
     l) log_dir="${OPTARG}" ;;
     r) num_reps="${OPTARG}" ;;
     b) backend="${OPTARG}" ;;
-    d) reorder_dims="${OPTARG}" ;;
+    d) mode="${OPTARG}" ;;
     k) selected_keys="${OPTARG}" ;;
     v) verbose=1 ;;
-    h) echo "Usage: $0 [-p enable_hyperthreading] [-e einsum_ir_executable] [-l log_directory] [-r num_repetitions] [-b backend] [-d reorder_dims] [-k benchmark_keys] [-v verbose]"
+    h) echo "Usage: $0 [-p enable_hyperthreading] [-e einsum_ir_executable] [-l log_directory] [-r num_repetitions] [-b backend] [-d mode] [-k benchmark_keys] [-v verbose]"
        echo "Optional arguments:"
        echo "  -p: Enable hyperthreading"
-       echo "  -e: Path to einsum executable"
+       echo "  -e: Path to einsum_ir executable"
        echo "  -l: Log directory"
        echo "  -r: Number of einsum_ir repetitions"
        echo "  -b: Backend to use (default: tpp)"
-       echo "  -d: Reorder dimensions (default: 1)"
+       echo "  -d: Mode in which the code is executed (default: 1)"
+       echo "      0: Use expr + contraction path, do NOT reorder dimensions"
+       echo "      1: Use expr + contraction path, do     reorder dimensions."
+       echo "      2: Use pre-optimized einsum tree."
        echo "  -k: Benchmark keys to run (default: all, options: syn,tt,fctn,tw,getd,trn,mera,tnlm,tccg_blocked,tccg_blocked_reordered,fc)"
        echo "  -v: Verbose output (default: off)"; exit 0 ;;
     *) echo "Unexpected option ${flag}"
-       echo "Usage: $0 [-p enable_hyperthreading] [-e einsum_ir_executable] [-l log_directory] [-r num_repetitions] [-b backend] [-d reorder_dims] [-k benchmark_keys]"
+       echo "Usage: $0 [-p enable_hyperthreading] [-e einsum_ir_executable] [-l log_directory] [-r num_repetitions] [-b backend] [-d mode] [-k benchmark_keys]"
        exit 1 ;;
   esac
 done
@@ -39,7 +42,8 @@ selected_keys="${selected_keys:-all}"
 num_threads=$((num_cores * (1 + enable_ht)))
 num_reps=${num_reps:-1}
 backend="${backend:-tpp}"
-reorder_dims="${reorder_dims:-1}"
+mode="${mode:-1}"
+verbose="${verbose:-0}"
 
 # validate backend
 case "${backend}" in
@@ -47,10 +51,10 @@ case "${backend}" in
   *) echo "Error: Invalid backend '${backend}'. Must be tpp, blas, or tblis"; exit 1 ;;
 esac
 
-# validate reorder_dims
-case "${reorder_dims}" in
-  0|1) ;;
-  *) echo "Error: Invalid reorder_dims '${reorder_dims}'. Must be 0 or 1"; exit 1 ;;
+# validate mode
+case "${mode}" in
+  0|1|2) ;;
+  *) echo "Error: Invalid mode '${mode}'. Must be 0, 1 or 2"; exit 1 ;;
 esac
 
 # check if einsum_ir_exe exists
@@ -81,26 +85,39 @@ echo "  Log directory: ${log_dir}"
 echo "  KMP_AFFINITY: ${kmp_affinity}"
 echo "  Number of repetitions: ${num_reps}"
 echo "  Backend: ${backend}"
-echo "  Reorder dims: ${reorder_dims}"
+echo "  Mode: ${mode}"
 echo "  Selected benchmark keys: ${selected_keys}"
 echo ""
 
 declare -A settings
-settings["syn"]="einsum_benchmark/synthetic.cfg"
-settings["tt"]="tensor_decomp/tt.cfg"
-settings["fctn"]="tensor_decomp/fctn.cfg"
-settings["tw"]="tensor_decomp/tw.cfg"
-settings["getd"]="tensor_decomp/getd.cfg"
-settings["trn"]="tensor_layers/trn.cfg"
-settings["mera"]="einsum_benchmark/str_nw_mera_open_26.cfg"
-settings["tnlm"]="einsum_benchmark/lm_first_last_brackets_4_16d.cfg"
-settings["tccg_blocked_reordered"]="tccg/settings_blocked_reordered.cfg"
-settings["tccg_blocked"]="tccg/settings_blocked.cfg"
-settings["fc"]="tensor_layers/fc.cfg"
+if [ "$mode" = "0" ] || [ "$mode" = "1" ]; then
+  settings["syn"]="einsum_benchmark/synthetic.cfg"
+  settings["tt"]="tensor_decomp/tt.cfg"
+  settings["fctn"]="tensor_decomp/fctn.cfg"
+  settings["tw"]="tensor_decomp/tw.cfg"
+  settings["getd"]="tensor_decomp/getd.cfg"
+  settings["trn"]="tensor_layers/trn.cfg"
+  settings["mera"]="einsum_benchmark/str_nw_mera_open_26.cfg"
+  settings["tnlm"]="einsum_benchmark/lm_first_last_brackets_4_16d.cfg"
+  settings["tccg_blocked_reordered"]="tccg/settings_blocked_reordered.cfg"
+  settings["tccg_blocked"]="tccg/settings_blocked.cfg"
+  settings["fc"]="tensor_layers/fc.cfg"
+else
+  settings["syn"]="einsum_benchmark/synthetic_et.cfg"
+  settings["tt"]="tensor_decomp/tt_et.cfg"
+  settings["fctn"]="tensor_decomp/fctn_et.cfg"
+  settings["tw"]="tensor_decomp/tw_et.cfg"
+  settings["getd"]="tensor_decomp/getd_et.cfg"
+  settings["trn"]="tensor_layers/trn_et.cfg"
+  settings["mera"]="einsum_benchmark/str_nw_mera_open_26_et.cfg"
+  settings["tnlm"]="einsum_benchmark/lm_first_last_brackets_4_16d_et.cfg"
+fi
 
+# get the directory where the script is located
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 for key in $(echo "${!settings[@]}" | tr ' ' '\n')
 do
-  settings[${key}]="einsum_ir/samples/${settings[${key}]}"
+  settings[${key}]="$(dirname "$script_dir")/${settings[${key}]}"
 done
 
 # check if config files exist
@@ -135,7 +152,12 @@ do
   echo "Running benchmark setting: ${settings[${key}]}"
 
   mkdir -p "${log_dir}/${key}"
-  log_file="${log_dir}/${key}/einsum_ir_backend_${backend}_reorder_dims_${reorder_dims}.log"
+
+  if [ "${mode}" = "0" ] || [ "${mode}" = "1" ]; then
+    log_file="${log_dir}/${key}/einsum_ir_backend_${backend}_reorder_dims_${mode}.log"
+  else
+    log_file="${log_dir}/${key}/einsum_ir_backend_${backend}.log"
+  fi
   cat /dev/null > "${log_file}"
 
   total_settings=$(grep -c '^' "${settings[${key}]}")
@@ -143,15 +165,19 @@ do
   while IFS= read -r setting || [ -n "$setting" ]; do
     ((current_setting++))
     echo -n "  Processing setting ${current_setting}/${total_settings}"
-    command="${einsum_ir_exe} ${setting} FP32 0 1"
+    if [ "${mode}" = "0" ] || [ "${mode}" = "1" ]; then
+      command="${einsum_ir_exe} ${setting} FP32 0 1"
+    else
+      command="${einsum_ir_exe} ${setting} FP32"
+    fi
     for rep in $(seq 1 ${num_reps})
     do
       if [ "${verbose}" -gt 0 ]; then
-        EINSUM_IR_BACKEND=${backend^^} EINSUM_IR_REORDER_DIMS=${reorder_dims} OMP_NUM_THREADS=${num_threads} KMP_AFFINITY=${kmp_affinity} eval ${command} | tee -a ${log_file}
+        EINSUM_IR_BACKEND=${backend^^} EINSUM_IR_REORDER_DIMS=${mode} OMP_NUM_THREADS=${num_threads} KMP_AFFINITY=${kmp_affinity} eval ${command} | tee -a ${log_file}
       else
-        EINSUM_IR_BACKEND=${backend^^} EINSUM_IR_REORDER_DIMS=${reorder_dims} OMP_NUM_THREADS=${num_threads} KMP_AFFINITY=${kmp_affinity} eval ${command} >> ${log_file} 2>&1
+        EINSUM_IR_BACKEND=${backend^^} EINSUM_IR_REORDER_DIMS=${mode} OMP_NUM_THREADS=${num_threads} KMP_AFFINITY=${kmp_affinity} eval ${command} >> ${log_file} 2>&1
+        echo -n "."
       fi
-      echo -n "."
     done
     echo ""
   done < "${settings[${key}]}"
